c352cb41da740683bd6db97b7e9fab5635acbe6c
ElodieBerthaud <elodie.berthaud@epitech.eu>
2023-04-02 16:09:49 +0200

    test: forgot loading state test

--- src/Components/UsersList/UsersList.test.tsx
+++ src/Components/UsersList/UsersList.test.tsx
@@ -60,17 +60,83 @@ describe('Users List | Users', () => {
     const onUserDeleted = jest.fn()
 
     const list = create(
-            <UsersList
-                users={users}
-                page={0}
-                perPage={5}
-                total={12}
-                totalPages={2}
-                onPaginationChange={onPaginationChange}
-                deleteUser={deleteUser}
-                isLoading={false}
-                onUserDeleted={onUserDeleted}
-            />
+        <UsersList
+            users={users}
+            page={0}
+            perPage={5}
+            total={12}
+            totalPages={2}
+            onPaginationChange={onPaginationChange}
+            deleteUser={deleteUser}
+            isLoading={false}
+            onUserDeleted={onUserDeleted}
+        />
+    )
+    const tree = list.toJSON()
+    expect(tree).toMatchSnapshot()
+  })
+
+  it('should render a loading state', function () {
+    const users = [
+      {
+        id: 1,
+        email: 'george.bluth@reqres.in',
+        first_name: 'George',
+        last_name: 'Bluth',
+        avatar: 'https://reqres.in/img/faces/1-image.jpg'
+      },
+      {
+        id: 2,
+        email: 'janet.weaver@reqres.in',
+        first_name: 'Janet',
+        last_name: 'Weaver',
+        avatar: 'https://reqres.in/img/faces/2-image.jpg'
+      },
+      {
+        id: 3,
+        email: 'emma.wong@reqres.in',
+        first_name: 'Emma',
+        last_name: 'Wong',
+        avatar: 'https://reqres.in/img/faces/3-image.jpg'
+      },
+      {
+        id: 4,
+        email: 'eve.holt@reqres.in',
+        first_name: 'Eve',
+        last_name: 'Holt',
+        avatar: 'https://reqres.in/img/faces/4-image.jpg'
+      },
+      {
+        id: 5,
+        email: 'charles.morris@reqres.in',
+        first_name: 'Charles',
+        last_name: 'Morris',
+        avatar: 'https://reqres.in/img/faces/5-image.jpg'
+      },
+      {
+        id: 6,
+        email: 'tracey.ramos@reqres.in',
+        first_name: 'Tracey',
+        last_name: 'Ramos',
+        avatar: 'https://reqres.in/img/faces/6-image.jpg'
+      }
+    ]
+    const onPaginationChange = jest.fn()
+    const deleteUser = jest.fn()
+    const onUserDeleted = jest.fn()
+
+    const list = create(
+        <UsersList
+            users={users}
+            page={0}
+            perPage={5}
+            total={12}
+            totalPages={2}
+            onPaginationChange={onPaginationChange}
+            deleteUser={deleteUser}
+            isLoading={true}
+            onUserDeleted={onUserDeleted}
+        />
     )
     const tree = list.toJSON()
     expect(tree).toMatchSnapshot()
@@ -125,17 +191,17 @@ describe('Users List | Users', () => {
     const deleteUser = jest.fn()
     const onUserDeleted = jest.fn()
     render(
-            <UsersList
-                users={users}
-                page={0}
-                perPage={5}
-                total={12}
-                totalPages={2}
-                onPaginationChange={onPaginationChange}
-                deleteUser={deleteUser}
-                isLoading={false}
-                onUserDeleted={onUserDeleted}
-            />)
+        <UsersList
+            users={users}
+            page={0}
+            perPage={5}
+            total={12}
+            totalPages={2}
+            onPaginationChange={onPaginationChange}
+            deleteUser={deleteUser}
+            isLoading={false}
+            onUserDeleted={onUserDeleted}
+        />)
     fireEvent.click(screen.getByTitle('Go to next page'))
     expect(onPaginationChange).toHaveBeenCalledWith(1, 5)
   })
